916b005c4fe5e668cfdae4b798caece60750e019
Align-center <lucasfrech@hotmail.fr>
2020-10-16 10:58:53 +0200

    Added canvas boundaries
    This is the first step for collisions. Setting boundaries.
    For now it is not configurable, but in the end I'd like to let the user choose which side of the canvas they want to set boundaries.
    
    To do so, you just have to specify if you want a shape to be affected by the boundaries. It is the last property of the constructor.
    
    Finnaly each shape will have its own setBoundaries method beacause of the differences of collision detection.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 class Shape {
 
-    constructor(x, y, vx, vy, color){
+    constructor(x, y, vx, vy, color, boundaries){
         this.x = x;
         this.y = y;
         this.vx = vx;
         this.vy = vy;
         this.color = color;
+        this.boundaries = boundaries;
+
         this.shapes= [];
     }
 
@@ -22,6 +24,18 @@ class Shape {
 
         this.y += this.vy;
         this.x += this.vx;
+
+        if (this.vy != 0) {
+
+            this.vy += .2;
+        }
+
+        for (let i = 0; i < instances.length; i++) {
+
+            if(instances[i].boundaries === true) {
+                instances[i].setBoundaries(canvas);
+            }
+        }
         
         window.requestAnimationFrame(this.moveShape.bind(this, ctx, canvas, instances));
     }
@@ -37,8 +51,8 @@ class Shape {
 
 class Ball extends Shape {
 
-    constructor(radius, x, y, vx = 0, vy = 5, color = 'black') {
-        super(x, y, vx, vy, color);
+    constructor(radius, x, y, vx = 0, vy = 5, color = 'black', boundaries) {
+        super(x, y, vx, vy, color, boundaries);
         this.radius = radius;
 
         super.instances(this);
@@ -51,12 +65,61 @@ class Ball extends Shape {
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx.fill();
     }
+
+    setBoundaries(canvas) {
+        
+        //Upper and Lower Boundaries
+        if (this.y + this.vy + this.radius > canvas.height) {
+                
+            if (this.vy < .6) {
+
+                this.vy = 0;
+            }
+            else {
+
+                this.vy = -this.vy;
+                this.vy *= .7;
+            }
+        }
+
+        if (this.y + this.vy - this.radius < 0) {
+
+            this.vy = - this.vy
+            this.vy *= .7;
+        }
+
+        //Sides Boundaries
+        if (this.x + this.vx + this.radius > canvas.width || this.x + this.vx - this.radius < 0) {
+            
+            if (this.vx < 2 && this.vx > -2) {
+
+                this.vx = 0;
+            }
+            else {
+
+                let number = .8
+                this.vx *= number;
+                this.vx = -this.vx;
+                number -= .3;
+            }  
+        }
+
+        //Make impossible for the ball the get out of the canvas
+        if (this.x + this.radius > canvas.width) {
+
+            this.x = canvas.width - this.radius;
+        }
+        else if (this.x - this.radius < 0) {
+
+            this.x = 0 + this.radius;
+        }
+    }
 }
 
 class Rectangle extends Shape{
 
-    constructor(width, height, x, y, vx = 0, vy = 5, color = 'black') {
-        super(x, y, vx, vy, color);
+    constructor(width, height, x, y, vx = 0, vy = 5, color = 'black', boundaries) {
+        super(x, y, vx, vy, color, boundaries);
         this.width = width;
         this.height = height;
 
